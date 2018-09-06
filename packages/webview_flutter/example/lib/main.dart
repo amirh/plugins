c2667d979bc6fd857ad6fb64ace2199ee2791ad8
Amir Hardon <amirha@google.com>
2018-09-06 10:57:28 -0700

    Example of a WebView and a TabBarView playing together.

--- packages/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/example/lib/main.dart
@@ -2,26 +2,74 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MaterialApp(home: new WebViewExample()));
 
-class WebViewExample extends StatelessWidget {
+class WebViewExample extends StatefulWidget {
+  @override
+  State createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  final List<String> tabs = <String>[
+    'https://flutter.io',
+    'https://news.google.com',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView example'),
+        bottom: new TabBar(
+          controller: tabController,
+          tabs: tabs.map((String url) => new Tab(text: url)).toList(),
+        ),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
         actions: <Widget>[const SampleMenu()],
       ),
-      body: const WebView(
-        initialUrl: 'https://youtube.com',
-        javaScriptMode: JavaScriptMode.unrestricted,
+      body: new TabBarView(
+        controller: tabController,
+        children: tabs.map((String url) =>  new WebViewTab(url: url)).toList(),
       ),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = new TabController(length: tabs.length, vsync: this);
+  }
+}
+
+class WebViewTab extends StatefulWidget {
+  const WebViewTab({Key key, this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  State createState() => new WebViewTabState();
+}
+
+class WebViewTabState extends State<WebViewTab> with AutomaticKeepAliveClientMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return new WebView(
+      initialUrl: widget.url,
+      javaScriptMode: JavaScriptMode.unrestricted,
+      gestureRecognizers: <OneSequenceGestureRecognizer>[ new VerticalDragGestureRecognizer() ],
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
 
 class SampleMenu extends StatelessWidget {
