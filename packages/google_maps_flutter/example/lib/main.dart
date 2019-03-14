84f9b75ddd48608654818242d0b9d550db7eae45
Amir Hardon <amirha@google.com>
2019-03-14 10:20:02 -0700

    debugging

--- packages/google_maps_flutter/example/lib/main.dart
+++ packages/google_maps_flutter/example/lib/main.dart
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'animate_camera.dart';
 import 'map_ui.dart';
 import 'move_camera.dart';
@@ -29,20 +31,58 @@ class MapsDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('GoogleMaps examples')),
-      body: ListView.builder(
-        itemCount: _allPages.length,
-        itemBuilder: (_, int index) => ListTile(
-              leading: _allPages[index].leading,
-              title: Text(_allPages[index].title),
-              onTap: () => _pushPage(context, _allPages[index]),
-            ),
-      ),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Glitch(),
+      // body: ListView.builder(
+      //   itemCount: _allPages.length,
+      //   itemBuilder: (_, int index) => ListTile(
+      //         leading: _allPages[index].leading,
+      //         title: Text(_allPages[index].title),
+      //         onTap: () => _pushPage(context, _allPages[index]),
+      //       ),
+      // ),
     );
   }
 }
 
 void main() {
+  timeDilation = 0.0001;
   runApp(MaterialApp(home: MapsDemo()));
+}
+
+
+class Glitch extends StatefulWidget {
+  @override
+  State<Glitch> createState() => GlitchState();
+}
+
+class GlitchState extends State<Glitch> {
+  bool showMap = false;
+
+  @override
+  Widget build(BuildContext context) {
+   return Column(
+     children: <Widget>[
+       Text('Hello'),
+       Container(
+         width: 300,
+         height: 300,
+         child: showMap ?
+         GoogleMap(
+           initialCameraPosition: CameraPosition(target: LatLng(0, 0)),
+         )
+             : null,
+       ),
+       GestureDetector(
+         onTap: () {
+           setState(() {
+             showMap = !showMap;
+           });
+         },
+         child: Text('TOGGLE SHOW MAP'),
+       ),
+     ],
+   );
+  }
 }
